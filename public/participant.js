7434aad6cb39f03435b7a3693702780bd47b9b9c
eapr225 <eapr225@uky.edu>
2016-09-27 17:54:25 -0400

    Update participant.js

--- public/participant.js
+++ public/participant.js
@@ -1,5 +1,5 @@
 function setup(){
-  createCanvas(400, 200);
+  createCanvas(800, 800);
   background(0);
   console.log("Ready to listen to Input -->");
 }
@@ -9,6 +9,11 @@ function draw(){
 
   rect(50, 100, 100, 50);
   rect(250, 100, 100, 50);
+  rect(450, 100, 100, 50);
+  rect(50, 200, 100, 50);
+  rect(250, 200, 100, 50);
+  rect(450, 200, 100, 50);
+
 }
 
 function mouseClicked() {
@@ -20,5 +25,25 @@ function mouseClicked() {
   if(mouseX>250&&mouseX<350&&mouseY>100&&mouseY<150){
     console.log("clicked on trigger_2");
     httpGet('/trigger_2');
+  }
+
+  if(mouseX>450&&mouseX<550&&mouseY>100&&mouseY<150){
+    console.log("clicked on trigger_3");
+    httpGet('/trigger_3');
+  }
+
+  if(mouseX>50&&mouseX<150&&mouseY>200&&mouseY<250){
+    console.log("clicked on trigger_4");
+    httpGet('/trigger_4');
+  }
+
+  if(mouseX>250&&mouseX<350&&mouseY>200&&mouseY<250){
+    console.log("clicked on trigger_5");
+    httpGet('/trigger_5');
+  }
+
+  if(mouseX>450&&mouseX<550&&mouseY>200&&mouseY<250){
+    console.log("clicked on trigger_6");
+    httpGet('/trigger_6');
   }
 }
