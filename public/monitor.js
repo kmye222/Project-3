9b1eb3a7cdea2ef450015cd9bf6d25014f087525
Jamie <jtlthe2@gmail.com>
2016-10-11 15:44:21 -0400

    Color added to participant. Shapes added to moniter.

--- public/monitor.js
+++ public/monitor.js
@@ -1,35 +1,118 @@
-var socket = io('http://127.0.0.1:8081');     //http://socket.io/docs/
+var pubnub = new PubNub({
+  subscribeKey : 'sub-c-f3ee9bac-8c06-11e6-a8c4-0619f8945a4f'
+})
+
+//var colour = color(255);
 var message_state = 0;
+var colour; //integer that contains the color to be sent to the moniter.
+var unit; //unit to multiply all screen demensional junk by to keep it all nice and ratioed.
+var x1, y1;
+var x2, y2;
+var x3, y3;
+var x4, y4;
+var x5, y5;
+var x6, y6;
+var wid, hei;
 
 function setup(){
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   background(0);
+  
+  unit = (windowWidth*windowHeight)/(700*1500);
+
+  wid = unit*495;
+  hei = unit*367.5;
+
+  x1 = unit*5;
+  y1 = unit*10;
+
+  x2 = unit*495;
+  y2 = unit*10;
+
+  x3 = unit*990;
+  y3 = unit*10;
+
+  x4 = unit*5;
+  y4 = unit*367.5;
+
+  x5 = unit*495;
+  y5 = unit*367.5;
+
+  x6 = unit*990;
+  y6 = unit*367.5;
+
+  console.log("Subscribing..");
+  pubnub.subscribe({
+    channels: ['coco_channel_no_1'] //Change this!!!
+  })
 }
 
 function draw(){
 
   background(0);
   textSize(20);
-  fill(255, 102, 153);
+  fill(255);
 
   //a very simple state machine
   if(message_state==0){
-    text("Under Construction", 100, 100);
+    
   }
   else if(message_state==1){
-    text("Trigger 1", 100, 100);
+    rect(x1, y1, wid, hei);
+  }
+  else if(message_state==2){
+    rect(x2, y2, wid, hei);
+  }
+  else if(message_state==3){
+    rect(x3, y3, wid, hei);
+  }
+  else if(message_state==4){
+    rect(x4, y4, wid, hei);
+  }
+  else if(message_state==5){
+    rect(x5, y5, wid, hei);
   }
   else{
-    text("Trigger 2", 100, 100);
+    rect(x6, y6, wid, hei);
   }
 }
 
-socket.on('trigger_1', function(){
-  console.log("trigger 1 listener fired");
-  message_state=1;
-})
 
-socket.on('trigger_2', function(){
-  console.log("trigger 2 listener fired");
-  message_state=2;
+pubnub.addListener({
+  message: function(command){
+    console.log(command);
+    if(command.message.box==1){
+      //colour = color(command.message.R, command.message.G, command.message.B);
+      message_state=1;
+      console.log("box 1 listener fired");
+    }
+    else if (command.message.box==2){
+      //colour = color(command.message.R, command.message.G, command.message.B);
+      message_state=2;
+      console.log("box 2 listener fired");
+    }
+    else if (command.message.box==3){
+      //colour = color(command.message.R, command.message.G, command.message.B);
+      message_state=3;
+      console.log("box 3 listener fired");
+    }
+    else if (command.message.box==4){
+      //colour = color(command.message.R, command.message.G, command.message.B);
+      message_state=4;
+      console.log("box 4 listener fired");
+    }
+    else if (command.message.box==5){
+      //colour = color(command.message.R, command.message.G, command.message.B);
+      message_state=5;
+      console.log("box 5 listener fired");
+    }
+    else if (command.message.box==6){
+      //colour = color(command.message.R, command.message.G, command.message.B);
+      message_state=6;
+      console.log("box 6 listener fired");
+    }
+    else{
+      message_state=0;
+    }
+  }
 })
